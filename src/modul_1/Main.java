cfd587528e1046b48ad67489a70119baeeaa797a
ridhoryanghifari <ridhoryanghifari@gmail.com>
2025-09-11 15:38:44 +0700

    inisiliasi 1

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package modul_1;
+
+public class Main {
+    // fungsi khusus sebagai entry point dari aplikasi yang dibuat
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
